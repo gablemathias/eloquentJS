d7aa902faba764f3b89728bd16676c08f72a7dd2
Gabriel Mathias <gablemathias@icloud.com>
2024-03-26 13:49:16 +1100

    own loop

---
+++ ex_chapter5/every.js
@@ -0,0 +1,24 @@
+// Loop version
+function every(array, test) {
+  if(array.length == 0) return test("");
+
+  let status = false;
+  for(value of array) {
+    status = test(value);
+    if(!status) break;
+  }
+  return status;
+}
+
+// Some version
+
+function every(array, test) {
+  return !array.some(element => !test(element));
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
