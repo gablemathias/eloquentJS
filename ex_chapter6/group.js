11c9f1d6815838011a01a015e5551614687732ee
Gabriel Mathias <gablemathias@icloud.com>
2024-06-18 18:06:10 -0300

    group like set

--- ex_chapter6/group.js
+++ ex_chapter6/group.js
@@ -1,72 +1,40 @@
 class Group {
-  constructor(group = []){
-    this.group = group;
+  constructor(){
+    this.group = [];
   }
 
   add(value){
-    if(!this.has(value)) this.group.push(value);
+    if(!this.has(value)){
+      this.group.push(value);
+    }
   }
 
   delete(value){
-    // if(this.has(value)){
-    //   this.group.splice(this.group.indexOf(value), 1)
-    // }
-    this.group = this.group.filter(elm => elm != value);
+    if(this.has(value)){
+      let indexOfValueToRemove = this.group.indexOf(value)
+      this.group.splice(indexOfValueToRemove, 1);
+    }
   }
 
   has(value){
     return this.group.includes(value);
   }
 
-  static from(array){
-    let unique = []
-    array.forEach(element => {
-      if(!unique.includes(element)){
-        unique.push(element);
-      }
-    });
-
-    return new this(unique);
-  }
-
-  [Symbol.iterator]() {
-    return new GroupIterator(this.group);
-  }
-}
-
-class GroupIterator {
-  #group;
-  #position;
-
-  constructor(group) {
-    this.#group = group;
-    this.#position = 0;
-  }
-
-  next() {
-    if (this.#position >= this.#group.length) {
-      return {done: true};
-    } else {
-      let result = {value: this.#group[this.#position],
-                    done: false};
-      this.#position++;
-      return result;
+  static from(values){
+    let set = new Group();
+    for (const value of values) {
+      set.add(value);
     }
+    return set;
   }
 }
 
-let group = Group.from([10, 20, 10]);
+let group = Group.from([10, 20]);
 console.log(group.has(10));
 // → true
 console.log(group.has(30));
 // → false
-group.add(150);
-console.log(group);
-group.delete(20);
+group.add(10);
+group.delete(10);
 console.log(group.has(10));
 // → false
-console.log(group);
-
-for (let value of Group.from(["a", "b", "c"])) {
-  console.log(value);
-}
